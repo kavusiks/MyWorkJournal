63de5fc46a5aba524c72781f137c76a3ce6f363d
SverreOK <sverre.o.k@hotmail.no>
2021-04-01 18:40:09 +0200

    Added some test to EmployeeTest class and changed promttext in startTime

--- myworkjournal/src/test/java/myworkjournal/core/tests/EmployeeTest.java
+++ myworkjournal/src/test/java/myworkjournal/core/tests/EmployeeTest.java
@@ -1,5 +1,63 @@
 package myworkjournal.core.tests;
 
-public class EmployeeTest {
+import myworkjournal.core.Employee;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EmployeeTest {
+	
+	@Test
+	public void testConstructor() {
+		// Check if name corresponds
+		Employee employee = new Employee("Ola");
+		Assertions.assertEquals("Ola", employee.getName());
+		
+		// Check if it throws exception when number is in name
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+	        Employee employee1 = new Employee("1234Ola");
+	    });
+		Exception exception2 = assertThrows(IllegalArgumentException.class, () -> {
+	        Employee employee3 = new Employee("");
+	    });
+		Exception exception3 = assertThrows(IllegalArgumentException.class, () -> {
+	        Employee employee4 = new Employee("'-e");
+	    });
+		
+	}
+	
+	@Test
+	public void testSetName() {
+		Employee employee = new Employee("Ola");
+		employee.setName("OlaNordmann");
+		Assertions.assertEquals("OlaNordmann", employee.getName());
+		employee.setName("Ola");
+		Assertions.assertEquals("Ola", employee.getName());
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+	        employee.setName("1234");
+	    });
+		Exception exception2 = assertThrows(IllegalArgumentException.class, () -> {
+	        employee.setName("");
+	    });
+		Exception exception3 = assertThrows(IllegalArgumentException.class, () -> {
+	        employee.setName("'-e");
+	    });
+		
+	}
+	
+	@Test
+	public void testAddWorkPeriod() {
+		// WIP
+	}
+	
+	@Test
+	public void mergeTwoWorkPeriods() {
+		// WIP
+	}
+	
+	@Test
+	public void testToString() {
+		// WIP
+	}
 }
