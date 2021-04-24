bba9a99a30fbf3ace76d8b82e78668c911cdc333
Sverre <sverreok@stud.ntnu.no>
2021-04-24 14:33:14 +0200

    code cleanup

--- myworkjournal/src/main/java/myworkjournal/core/Work.java
+++ myworkjournal/src/main/java/myworkjournal/core/Work.java
@@ -51,7 +51,6 @@ public class Work implements Comparable<Work> {
   public double getShiftDurationInHours() {
     DecimalFormat df = new DecimalFormat("###.##");
     df.setDecimalFormatSymbols(DecimalFormatSymbols.getInstance(Locale.ENGLISH));
-    double test = (double) Duration.between(getStartTime(), getEndTime()).toMinutes() / 60.00;
     return Double.valueOf(df.format((double) Duration.between(getStartTime(), getEndTime()).toMinutes() / 60.00));
   }
 
